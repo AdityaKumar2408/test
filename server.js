e75257f77c255ac44ca1eadaae118b64755bc649
AdityaKumar2408 <adityachouhan7705@gmail.com>
2024-11-03 12:30:12 +0530

    update scrips

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const io = new Server(server);
 
 app.use(express.static('build'));
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const userSocketMap = {};
